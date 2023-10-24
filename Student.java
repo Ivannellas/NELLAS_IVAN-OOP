8927cd5f30332c3536fe2ff324c6674c5fd126c2
Ivannellas <113983048+Ivannellas@users.noreply.github.com>
2023-10-24 12:59:17 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,40 @@
+package Student;
+
+class Student{
+   
+   String firstName = "";
+   String middleName = "";
+   String lastName = "";
+   String suffix = "";
+
+
+   public String getfirstName(String){
+      return = firstName;
+   }
+   
+   public String getmiddleName(String){
+      return = middleName;
+      }
+   public String getlastName(String){
+      return = lastName;
+      }
+   public String getsuffix(String){
+      return = suffix;
+      }
+    
+     
+     public void setfirstName(String fname){
+      firstName = fname;
+     }
+     public void setlasttName(String Lname){
+      lastName = Lname;
+     }
+    public void setmiddleName(String Mname){
+      middleName = Mname;
+     }
+      public void suffix(int suff){
+      suffix = suff;
+     }
+
+            
+}
